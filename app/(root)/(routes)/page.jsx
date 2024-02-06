4e08c8b37632b5c696c243e7a107f3170073eb2c
Nakwonyoon <yoonnw1000@gmail.com>
2024-02-06 17:18:48 +0900

    add sheet and search

--- app/(root)/(routes)/page.jsx
+++ app/(root)/(routes)/page.jsx
@@ -2,7 +2,7 @@ import { SearchInput } from "@/components/search-input"
 
 const RootPage = () => {
   return (
-    <div>
+    <div className="h-full p-4 space-y-2">
       <SearchInput />
     </div>
   )
--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,10 +1,11 @@
 "use client"
 import { Input } from "@/components/ui/input"
+import { Search } from "lucide-react"
 
 export const SearchInput = () => {
     return(
-        <div className="flex items-center">
-           <Input/>
+        <div className="relative">
+           <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground"/>
         </div>
     )
 }
